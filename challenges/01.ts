ab6e579a6898831705e26b88843a9e5b68b5137f
@erick <erick.hx22@gmail.com>
2022-12-06 13:26:49 -0500

    feat: add challenge 01 (#1)

---
+++ challenges/01.ts
@@ -0,0 +1,16 @@
+const gifts = ["book", "game", "socks"];
+
+function wrapping(gifts: string[]) {
+  const paperLength = (giftLength: number) => "*".repeat(giftLength + 2);
+  const wrapGift = (gift: string) =>
+    [paperLength(gift.length), `*${gift}*`, paperLength(gift.length)].join(
+      "\n"
+    );
+  const wrappedGifts = gifts.map((gift: string) => wrapGift(gift));
+
+  return wrappedGifts;
+}
+
+const wrapped = wrapping(gifts);
+
+console.log(wrapped);
