94b2882240be9de810fb71f6e7cd61e19e4d55f3
@erick <erick.hx22@gmail.com>
2022-12-16 18:35:29 -0500

    feat: add 02 challenge (#3)

---
+++ challenges/02.ts
@@ -0,0 +1,26 @@
+const year = 1985;
+const holidays = [
+  "01/01",
+  "01/06",
+  "02/02",
+  "02/17",
+  "02/28",
+  "06/03",
+  "12/06",
+  "12/25",
+]; // format MM/DD
+
+function countHours(year: number, holidays: string[]) {
+  const findHolidays = holidays
+    .map((h) => {
+      const date = Date.parse(`${h}/${year}`);
+      const newDate = new Date(date);
+      const dayNumber = newDate.getDay();
+      return dayNumber;
+    })
+    .filter((value) => ![6, 0].includes(value));
+  const count = findHolidays.length ? findHolidays.length * 2 : 0;
+  return count;
+}
+
+const hours = countHours(year, holidays);
