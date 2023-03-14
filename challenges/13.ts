b5a1c584e2c154c767ae9940c1b54a15cb600a2c
@erick <erick.hx22@gmail.com>
2023-03-14 15:29:23 -0500

    feat: add solution for challenge 13 (#8)

---
+++ challenges/13.ts
@@ -0,0 +1,22 @@
+const lastBackup = 1546300800;
+
+const changes = [
+  [3, 1546301100],
+  [2, 1546300800],
+  [1, 1546300800],
+  [1, 1546300900],
+  [1, 1546301000],
+] as [number, number][];
+
+function getFilesToBackup(lastBackup: number, changes: [number, number][]) {
+  const result: number[] = changes.reduce((prev: any, curr, i) => {
+    const isAfter = curr[1] > lastBackup;
+    const isRepeated = prev.includes(curr[0]);
+    return prev.concat(isAfter && !isRepeated ? curr[0] : []);
+  }, []);
+  return result.sort((a, b) => a - b);
+}
+
+const logGetFilesToBackup = getFilesToBackup(lastBackup, changes);
+
+console.log(logGetFilesToBackup, "result");
